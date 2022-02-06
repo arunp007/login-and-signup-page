30155e4cbf1b3db322fb34d7108db4153a68b330
arunp007 <arunp4624@gmail.com>
2022-02-06 18:35:13 +0530

    Version 1

--- loginpage/loginpage/settings.py
+++ loginpage/loginpage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-epw25id#fecuqouj!fk)a0$pj*mskzduigsp)5y0$7^#nh)%j@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
