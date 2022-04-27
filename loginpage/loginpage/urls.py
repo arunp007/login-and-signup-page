7b63fca2e4fd7c914e39651c5183838ddff2edfb
arunp007 <arunp4624@gmail.com>
2022-04-27 22:38:05 +0530

    Version 1

--- loginpage/loginpage/urls.py
+++ loginpage/loginpage/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('logapp/', include ('logapp.urls')),
+    path('logapp/', include ('logapp.urls'))
 ]
